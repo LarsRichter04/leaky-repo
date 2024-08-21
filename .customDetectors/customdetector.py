474116cba958efe4cd162dc3259c215a5026be1f
Lars Richter <richtlrs@schaeffler.com>
2024-08-21 11:13:32 +0200

    reordered folders

--- .customDetectors/customdetector.py
+++ .customDetectors/customdetector.py
@@ -5,11 +5,13 @@ from detect_secrets.plugins.base import RegexBasedDetector
 
 
 class CustomWrittenDetector(RegexBasedDetector):
-    """Scans for Basic Auth formatted URIs."""
     secret_type = 'CustomWrittenDetector'
 
     denylist = [
         re.compile(
             r'([A-Z]\!)+'
+        ),
+        re.compile(
+            r'.{0,20}?(NONCE_SALT|LOGGED_IN_SALT|AUTH_SALT|NONCE_KEY|DB_PASSWORD|AUTH_KEY|SECURE_AUTH_KEY|LOGGED_IN_KEY).{0,20}?[\'\"].{10,120}[\'\"]'
         ),
     ]
